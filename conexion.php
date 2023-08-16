72f264fedef3f096f5f78e1769a37a0a48619800
Bustos Lautaro <lbustos@embrace.com>
2023-08-16 15:19:53 -0300

    cambio de info de db

--- conexion.php
+++ conexion.php
@@ -2,22 +2,22 @@
 function Conexion()
 {
     $host = "localhost";
-    $user = "id21149302_clientesembrace";
+    $user = "id21154007_embrace";
     $pass = "Embrace2023!";
-    $db = "id21149302_clientes";
+    $db = "id21154007_formularioencuesta";
 
     $conexion = mysqli_connect($host, $user, $pass, $db);
 
     if ($conexion) {
-        $query = "SELECT * FROM clientesEncuesta";
+        $query = "SELECT * FROM formulario_prueba";
         $result = mysqli_query($conexion, $query);
 
         if ($result){
             while ($row = mysqli_fetch_assoc($result)){
-                $ticket_id = $row["TICKET_ID"];
-                $url = "https://formularioembrace.000webhostapp.com/muestra_ticket.php?ticket=$ticket_id";
+                $ticket_id = $row["TKT"];
+                $url = "https://formularioembrace23.000webhostapp.com/muestra_ticket.php?ticket=$ticket_id";
 
-                $updateQuery = "UPDATE clientesEncuesta SET URL = '$url' WHERE TICKET_ID = '$ticket_id'";
+                $updateQuery = "UPDATE formulario_prueba SET URL = '$url' WHERE TKT = '$ticket_id'";
                 mysqli_query($conexion, $updateQuery);
                 echo "<h3>$ticket_id</h3>";
                 echo "<a href=\"$url\">Encuesta a $ticket_id</a><br>";
